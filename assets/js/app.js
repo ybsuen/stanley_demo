ad396fb2c8fa752e789664d0509a9966203e157e
Bernard Suen <bsysin@gmail.com>
2018-01-12 10:11:21 +0800

    Update menu structure with cross references of visualization examples

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,27 +18,20 @@ $(document).ready(function() {
   });
 
   $("#getdata").click(function(){
-     // $.getJSON('articles.json', function(obj) {
-        // console.log(obj);
-     // $.getJSON('document.json', function(obj) {
-     // $.getJSON('document2.json', function(obj) { 
-      // $.getJSON('http://localhost/d756a/fehd-json-view2.json', function(obj) { 
-      // $.getJSON('http://dev-com5961a.pantheonsite.io/fehd-json-view2', function(obj) {   
-      $.getJSON('http://localhost/d756a/product_count.json', function(obj) {
+ 
+      $("#box1").html('You an check out the product table from the menu. The red "Get Data" button below will let you aggregate the items by product category. <br/><br/>');
+      $.getJSON('http://dev-com5961a.pantheonsite.io/product_count.json', function(obj) {  
       $("#lesson").append("<ul>");
-      // $.each(obj, function(key, value) {
-      // $.each(obj.statuses, function(key,value) {
         $.each(obj.json, function(key,value) {
         $("#box1").append("<li>" + value.x + "(" + value.Total_Count + ")" + "</li>");
-        // $("#lesson").append("<li>"+value+"</li>");
-      });
-      $("#box1").append("</ul>");
+      }); 
+      $("#box1").append("<br /><a href='table.html' style='color:black;'><button>Click here to see the details in a table.</button></a></ul>");
       
     });
   }); /* End getdata */
 
   $("#cleardata").click(function(){
-    $("#box1").html('');
+    $("#box1").html('You an check out the product table from the menu. The red "Get Data" button below will let you aggregate the items by product category. <br/><br/>');
   });
   
         // alert("Menu clicked!");      
